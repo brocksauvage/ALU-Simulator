d654720046bb4990341f9847dd8f7766554287f8
Connor <kudude@att.net>
2017-05-03 15:10:38 -0500

    SLL SLR SLA ls

--- AltBuild/ALUSimulator.c
+++ AltBuild/ALUSimulator.c
@@ -27,14 +27,23 @@ void InternalSwitch( RegisterFile theRegisterFile,
                     uint32_t ShiftAmt,
                     uint32_t FunctionCodeResult,
                     uint32_t ImmediateValue ) {
-    
+
     switch(FunctionCodeResult)
     {
         case 0: //SLL
+            result = theRegisterFile[Rt] << ShiftAmt;
+            RegisterFile_Read(theRegisterFile, Rs, &theRegisterFile[Rs], Rt, &theRegisterFile[Rt]);
+            RegisterFile_Write(theRegisterFile, 1, Rd, storeVal);
             break;
         case 2: //SRL
+          result = theRegisterFile[Rt] >> ShiftAmt;
+            RegisterFile_Read(theRegisterFile, Rs, &theRegisterFile[Rs], Rt, &theRegisterFile[Rt]);
+            RegisterFile_Write(theRegisterFile, 1, Rd, storeVal);
             break;
         case 3: //SRA
+            result = (signed)theRegisterFile[Rt] >> ShiftAmt;
+            RegisterFile_Read(theRegisterFile, Rs, &theRegisterFile[Rs], Rt, &theRegisterFile[Rt]);
+            RegisterFile_Write(theRegisterFile, 1, Rd, storeVal);
             break;
         case 4: //SLLV
             break;
@@ -70,7 +79,7 @@ unsigned generateMask(unsigned a, unsigned b)
     unsigned i;
     for (i=a; i<=b; i++)
         r |= 1 << i;
-    
+
     return r;
 }
 
@@ -81,48 +90,48 @@ extern void ALUSimulator( RegisterFile theRegisterFile,
                          uint32_t FunctionCode,
                          uint32_t ImmediateValue,
                          uint32_t* Status ) {
-    
+
     printf( ">>ALU: Opcode: %02X; Rs: %02X; Rt: %02X; Rd: %02X;\n",
            OpCode,
            Rs,
            Rt,
            Rd );
-    
+
     unsigned int OpCodeResult;
     unsigned int mask;
-		
+
     mask = generateMask(0,5); // Generates mask for last 6 bits.
     OpCodeResult = mask & OpCode; // Applies mask. The OpCodeResult variable contains last 6 bits
-    
+
     unsigned int FunctionCodeResult = mask & FunctionCode;
     unsigned int result;
-    
+
     uint32_t compBool = 0;
     uint32_t storeVal;
-    
+
     switch(OpCodeResult)
     {
         case 0: //NOOP
             InternalSwitch(theRegisterFile, OpCodeResult, Rs, Rt, Rd, ShiftAmt, FunctionCodeResult, ImmediateValue);
             break;
         case 8: //ADDI
-            
+
             storeVal = (signed)((signed)theRegisterFile[Rs] + (signed)(int32_t)(int16_t)ImmediateValue);
-            
+
             RegisterFile_Read(theRegisterFile, Rs, &theRegisterFile[Rs], Rt, &theRegisterFile[Rt]);
             RegisterFile_Write(theRegisterFile, 1, Rt, storeVal);
-            
+
             break;
         case 9: //ADDIU
-            
+
             storeVal = (unsigned)((unsigned)theRegisterFile[Rs] + (unsigned)(int32_t)(int16_t)ImmediateValue);
-            
+
             RegisterFile_Read(theRegisterFile, Rs, &theRegisterFile[Rs], Rt, &theRegisterFile[Rt]);
             RegisterFile_Write(theRegisterFile, 1, Rt, storeVal);
-            
+
             break;
         case 10: //SLTI
-            
+
             if((signed)theRegisterFile[Rs] < (signed)(int32_t)(int16_t)ImmediateValue)
             {
                 compBool = 1;
@@ -131,13 +140,13 @@ extern void ALUSimulator( RegisterFile theRegisterFile,
             {
                 compBool = 0;
             }
-            
+
             RegisterFile_Read(theRegisterFile, Rs, &theRegisterFile[Rs], Rt, &theRegisterFile[Rt]);
             RegisterFile_Write(theRegisterFile, 1, Rt, compBool);
-            
+
             break;
         case 11: //SLTIU
-            
+
             if((unsigned)theRegisterFile[Rs] < (signed)(int32_t)(int16_t)ImmediateValue)
             {
                 compBool = 1;
@@ -146,19 +155,19 @@ extern void ALUSimulator( RegisterFile theRegisterFile,
             {
                 compBool = 0;
             }
-            
+
             RegisterFile_Read(theRegisterFile, Rs, &theRegisterFile[Rs], Rt, &theRegisterFile[Rt]);
             RegisterFile_Write(theRegisterFile, 1, Rt, compBool);
-            
+
             break;
         default:
             printf("Invalid OpCode!");
             break;
     }
-				
+
     printf( ">>>>ALU: ShiftAmt: %02X; FunctionCode: %02X; ImmediateValue: %04X;\n",
            ShiftAmt,
            FunctionCode,
            ImmediateValue );
-				
+
 }
